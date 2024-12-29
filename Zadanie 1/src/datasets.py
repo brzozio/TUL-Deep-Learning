210347f189ce8e9a7625ec12692a851adffb713d
brzozio <michal.brzozowski@interia.eu>
2024-12-29 19:05:07 +0100

    initial commit in the deep learning repo

---
+++ Zadanie 1/src/datasets.py
@@ -0,0 +1,17 @@
+from torchvision.datasets import CelebA
+import torchvision.transforms as transforms
+import os
+
+SRC_PATH : str  = os.path.dirname(os.path.abspath(__file__))
+CSV_PATH : str  = os.path.dirname(SRC_PATH) + '\\csv'
+JSON_PATH : str = os.path.dirname(SRC_PATH) + '\\json'
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+celeba_train = CelebA(root=CSV_PATH, split='train', download=True, transform=transform)
+celeba_val   = CelebA(root=CSV_PATH, split='valid', download=True, transform=transform)
+celeba_test  = CelebA(root=CSV_PATH, split='test', download=True, transform=transform)
