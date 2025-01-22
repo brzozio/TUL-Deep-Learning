78c5c5d7e5ed5f55b9cc4d3d4b0daa187ef4085e
brzozio <michal.brzozowski@interia.eu>
2025-01-22 22:17:00 +0100

    working

--- Zadanie 3/src/main.py
+++ Zadanie 3/src/main.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as plt
 from torch_geometric.nn import TransformerConv
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
-from model import GNNModelRegression,  GNNModelClassification as GNNModel
+from model import GNNModelRegression, GNNModelRegression_GCNConv,  GNNModelClassification, GNNModelClassification_GCNConv
+import numpy as np
 
 # Paths and Constants
 SRC_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -22,15 +23,14 @@ CSV_PATH = os.path.join(os.path.dirname(SRC_PATH), "csv")
 MODELS_PATH = os.path.join(os.path.dirname(SRC_PATH), "models")
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-BATCH_SIZE = 32
-LEARNING_RATE = 0.001
-EPOCHS = 100
-PATIENCE = 10
 
-TRAIN = False
-CONTINUE_TRAIN = True
-# MODEL_NAME = "classification"
-MODEL_NAME = "regression"
+BATCH_SIZE:     int   = 128
+LEARNING_RATE:  float = 1e-4
+TRAIN:          bool = False
+CONTINUE_TRAIN: bool = False
+
+# MODEL_NAME: str = "classification"
+MODEL_NAME: str  = "regression"
 
 class DipoleMomentTransform(BaseTransform):
             def __call__(self, data):
@@ -38,20 +38,55 @@ class DipoleMomentTransform(BaseTransform):
                 data.y = data.y[:, 4].view(-1, 1)  # Ensure shape [N, 1]
                 return data
 
-def plot_losses(train_losses, val_losses):
+def plot_losses(train_losses, val_losses, modelname):
     """Plot training and validation loss curves."""
     plt.figure(figsize=(10, 6))
     plt.plot(train_losses, label="Train Loss")
     plt.plot(val_losses, label="Validation Loss")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
-    plt.title("Training and Validation Loss Over Epochs")
+    plt.title(modelname)
     plt.legend()
     plt.grid()
-    plt.show()
+    plt.savefig(CSV_PATH+f"\\Losses_{modelname}.jpg")
+
+def plot_decision_boundary(X, func, modelname, y_true=None)-> None:
+    try:
+        fig, ax = plt.subplots()
+        # Definiujemy zakres dla osi x i y
+        x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+        y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+        
+        # Tworzymy siatkę punktów w celu wygenerowania granicy decyzyjnej
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                            np.arange(y_min, y_max, 0.1))
+        
+        # Obliczamy etykiety dla każdego punktu w siatce
+        meshgrid_tensor = torch.tensor(np.c_[xx.ravel(), yy.ravel()], dtype=torch.double)
+        Z = func(meshgrid_tensor)
+        Z = torch.argmax(Z,dim=1).detach().numpy().reshape(xx.shape)
+        print(Z)
+
+        #Predykcja etykiet dla danych testowych podanych w funkcji
+        if y_true is None:
+            y_tested = torch.argmax(func(X), dim=1).numpy()
+        else: y_tested=y_true
+
+        # Rysujemy kontury granicy decyzyjnej
+        ax.contourf(xx, yy, Z, alpha=0.4)
+        
+        # Rysujemy punkty treningowe
+        ax.scatter(X[:, 0], X[:, 1], c=y_tested, s=20, edgecolors='k')
+        
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.title(modelname)
+        plt.savefig(CSV_PATH+f"\\Decision_Boundary_{modelname}.jpg")
+    except Exception as e:
+        print(f"Failed to do decision boundary for : {modelname} with an error: {e}")
 
 class FaceIDModel(pl.LightningModule):
-    def __init__(self, model, train_loader, val_loader, task_type='classification', lr=1e-4):
+    def __init__(self, model, train_loader, val_loader, task_type, lr=1e-4):
         """
         task_type: 'classification' for classification tasks, 'regression' for regression tasks
         """
@@ -141,12 +176,12 @@ def get_data(dataset_name, task_type):
     print(f"edge_index type: {type(train_dataset[0].edge_index)} | shape: {train_dataset[0].edge_index.shape if isinstance(train_dataset[0].edge_index, torch.Tensor) else 'Not a tensor'}")
 
     return (
-        GeoDataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=8),
-        GeoDataLoader(val_dataset, batch_size=BATCH_SIZE, num_workers=8),
-        GeoDataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=8)
+        GeoDataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=6, persistent_workers=True),
+        GeoDataLoader(val_dataset, batch_size=BATCH_SIZE, num_workers=6, persistent_workers=True),
+        GeoDataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=6)
     )
 
-def train_model(model):
+def train_model(model, modelname):
     """Train the PyTorch Lightning model."""
     train_losses, val_losses = [], []
 
@@ -166,11 +201,19 @@ def train_model(model):
 
     checkpoint_callback = ModelCheckpoint(
         dirpath=MODELS_PATH,
-        filename=f"{MODEL_NAME}_{{epoch:03d}}-{{val_loss:.4f}}",
+        filename=f"{modelname}_{{epoch:03d}}-{{val_loss:.4f}}",
         monitor="val_loss",
         save_top_k=1,
         mode="min",
     )
+
+    if "regression" in modelname:
+        EPOCHS:         int   = 100
+        PATIENCE:       int   = 3
+    else:
+        EPOCHS:         int   = 400
+        PATIENCE:       int   = 10
+
 
     early_stopping = EarlyStopping(monitor="val_loss", patience=PATIENCE, verbose=True, mode="min")
 
@@ -185,81 +228,199 @@ def train_model(model):
 
     return train_losses, val_losses
 
+def main_transformer_conv() -> None:
+
+    EMBEDDINGS:     list  = [1,2,32]
+    LINEAR_REGRESSION:   list  = [True, False]
+    # MODEL_NAME:     list  = ["regression", "classification"]
+    MODEL_NAME:     list  = ["classification"]
+
+    for model_name_id in MODEL_NAME:
+        loop_count: int = len(LINEAR_REGRESSION) if model_name_id == "regression" else 1
+        for embed_id in EMBEDDINGS:
+
+            for linear_id in range(loop_count):
+
+                if model_name_id == "classification":
+                    train_loader, val_loader, test_loader = get_data('BACE', 'classification')
+                    model_core = GNNModelClassification(embedding=embed_id).to(DEVICE)
+                    saveModelName: str = f"TransformerConv_{model_name_id}_{embed_id}"
+                    
+                elif model_name_id == "regression":
+                    train_loader, val_loader, test_loader = get_data('QM9', 'regression')
+                    model_core = GNNModelRegression(embedding=embed_id, linear=LINEAR_REGRESSION[linear_id]).to(DEVICE)
+                    saveModelName: str = f"TransformerConv_{model_name_id}_{embed_id}_{'liniowy' if LINEAR_REGRESSION[linear_id] == True else 'nieliniowy'}"
+
+                model = FaceIDModel(model=model_core, train_loader=train_loader, val_loader=val_loader, task_type=model_name_id, lr=LEARNING_RATE)
+
+                if TRAIN:
+                    print(f"Running a training for {saveModelName}")
+                    if CONTINUE_TRAIN:
+                        checkpoint_path = os.path.join(MODELS_PATH, f"{model_name_id}.ckpt")
+                        model = FaceIDModel.load_from_checkpoint(checkpoint_path, model=model_core, train_loader=train_loader, val_loader=val_loader, task_type=model_name_id, lr=LEARNING_RATE)
+                    train_losses, val_losses = train_model(model, saveModelName)
+                    plot_losses(train_losses, val_losses, saveModelName)
+                else:
+                    checkpoint_path = MODELS_PATH + f"\\{saveModelName}.ckpt"
+
+                    model = FaceIDModel.load_from_checkpoint(checkpoint_path, model=model_core, train_loader=train_loader, val_loader=val_loader, task_type=model_name_id, lr=LEARNING_RATE)
+
+                    criterion = nn.BCEWithLogitsLoss() if model_name_id == 'classification' else nn.MSELoss()
+                    model.criterion = criterion
+
+                    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+                    model = model.to(device)
+
+                    model.eval()
+                    model.freeze()
+
+                    all_predictions = []
+                    all_targets = []
+
+                    with torch.no_grad():
+                        for batch in test_loader:
+                            x, y = batch.x.to(device), batch.y.to(device)
+                            edge_index = batch.edge_index.to(device)
+
+                            outputs = model(batch.to(device))
+
+                            if model_name_id == 'classification':
+                                y = y.squeeze().long()
+                                loss = nn.CrossEntropyLoss()(outputs, y)
+                                probabilities = torch.softmax(outputs, dim=-1)
+                                predictions = probabilities.argmax(dim=-1)
+
+                            elif model_name_id == 'regression':
+                                y = y.squeeze().float()
+                                loss = nn.MSELoss()(outputs.squeeze(), y)
+                                predictions = outputs.squeeze()
+
+                            all_predictions.append(predictions.cpu())
+                            all_targets.append(y.cpu())
+
+                    all_predictions = torch.cat(all_predictions)
+                    all_targets = torch.cat(all_targets)
+
+                    if model_name_id == 'classification':
+                        test_accuracy = (all_predictions == all_targets).float().mean().item() * 100
+                        print(f"Test Accuracy: {test_accuracy:.4f}%")
+
+                        cm = confusion_matrix(all_targets, all_predictions)
+                        plt.figure(figsize=(8, 6))
+                        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+                        plt.xlabel('Predicted Labels')
+                        plt.ylabel('True Labels')
+                        plt.title(saveModelName)
+                        plt.show()
+
+                    elif model_name_id == 'regression':
+                        mse = torch.mean((all_predictions - all_targets) ** 2).item()
+                        print(f"Mean Squared Error (MSE) {saveModelName}: {mse:.4f}")
+
+                model            = None
+                train_loader     = None
+                val_loader       = None
+                saveModelName    = None
+                test_loader      = None
+                model_core       = None
+
+def main_gcn_conv() -> None:
+
+    EMBEDDINGS:     list  = [1,2,32]
+    LINEAR_REGRESSION:   list  = [True, False]
+    # MODEL_NAME:     list  = ["regression", "classification"]
+    MODEL_NAME:     list  = ["classification"]
+
+    for model_name_id in MODEL_NAME:
+        loop_count: int = len(LINEAR_REGRESSION) if model_name_id == "regression" else 1
+        for embed_id in EMBEDDINGS:
+
+            for linear_id in range(loop_count):
+
+                if model_name_id == "classification":
+                    train_loader, val_loader, test_loader = get_data('BACE', 'classification')
+                    model_core = GNNModelClassification_GCNConv(embedding=embed_id).to(DEVICE)
+                    saveModelName: str = f"GCNConv_{model_name_id}_{embed_id}"
+                    
+                elif model_name_id == "regression":
+                    train_loader, val_loader, test_loader = get_data('QM9', 'regression')
+                    model_core = GNNModelRegression_GCNConv(embedding=embed_id, linear=LINEAR_REGRESSION[linear_id]).to(DEVICE)
+                    saveModelName: str = f"GCNConv_{model_name_id}_{embed_id}_{'liniowy' if LINEAR_REGRESSION[linear_id] == True else 'nieliniowy'}"
+
+                model = FaceIDModel(model=model_core, train_loader=train_loader, val_loader=val_loader, task_type=model_name_id, lr=LEARNING_RATE)
+
+                if TRAIN:
+                    print(f"Running a training for {saveModelName}")
+                    if CONTINUE_TRAIN:
+                        checkpoint_path = os.path.join(MODELS_PATH, f"{model_name_id}.ckpt")
+                        model = FaceIDModel.load_from_checkpoint(checkpoint_path, model=model_core, train_loader=train_loader, val_loader=val_loader, task_type=model_name_id, lr=LEARNING_RATE)
+                    train_losses, val_losses = train_model(model, saveModelName)
+                    plot_losses(train_losses, val_losses, saveModelName)
+                else:
+                    checkpoint_path = MODELS_PATH + f"\\{saveModelName}.ckpt"
+
+                    model = FaceIDModel.load_from_checkpoint(checkpoint_path, model=model_core, train_loader=train_loader, val_loader=val_loader, task_type=model_name_id, lr=LEARNING_RATE)
+
+                    criterion = nn.BCEWithLogitsLoss() if model_name_id == 'classification' else nn.MSELoss()
+                    model.criterion = criterion
+
+                    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+                    model = model.to(device)
+
+                    model.eval()
+                    model.freeze()
+
+                    all_predictions = []
+                    all_targets = []
+
+                    with torch.no_grad():
+                        for batch in test_loader:
+                            x, y = batch.x.to(device), batch.y.to(device)
+                            edge_index = batch.edge_index.to(device)
+
+                            outputs = model(batch.to(device))
+
+                            if model_name_id == 'classification':
+                                y = y.squeeze().long()
+                                loss = nn.CrossEntropyLoss()(outputs, y)
+                                probabilities = torch.softmax(outputs, dim=-1)
+                                predictions = probabilities.argmax(dim=-1)
+
+                            elif model_name_id == 'regression':
+                                y = y.squeeze().float()
+                                loss = nn.MSELoss()(outputs.squeeze(), y)
+                                predictions = outputs.squeeze()
+
+                            all_predictions.append(predictions.cpu())
+                            all_targets.append(y.cpu())
+
+                    all_predictions = torch.cat(all_predictions)
+                    all_targets = torch.cat(all_targets)
+
+                    if model_name_id == 'classification':
+                        test_accuracy = (all_predictions == all_targets).float().mean().item() * 100
+                        print(f"Test Accuracy: {test_accuracy:.4f}%")
+
+                        cm = confusion_matrix(all_targets, all_predictions)
+                        plt.figure(figsize=(8, 6))
+                        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+                        plt.xlabel('Predicted Labels')
+                        plt.ylabel('True Labels')
+                        plt.title(saveModelName)
+                        plt.show()
+
+                    elif model_name_id == 'regression':
+                        mse = torch.mean((all_predictions - all_targets) ** 2).item()
+                        print(f"Mean Squared Error (MSE) {saveModelName}: {mse:.4f}")
+
+                model            = None
+                train_loader     = None
+                val_loader       = None
+                saveModelName    = None
+                test_loader      = None
+                model_core       = None
+
 if __name__ == "__main__":
-    if MODEL_NAME == "classification":
-        train_loader, val_loader, test_loader = get_data('BACE', 'classification')
-        model_core = GNNModel().to(DEVICE)
-        
-    elif MODEL_NAME == "regression":
-        train_loader, val_loader, test_loader = get_data('QM9', 'regression')
-        model_core = GNNModelRegression().to(DEVICE)
-
-    model = FaceIDModel(model_core, train_loader, val_loader, MODEL_NAME, lr=LEARNING_RATE)
-
-    if TRAIN:
-        if CONTINUE_TRAIN:
-            checkpoint_path = os.path.join(MODELS_PATH, f"{MODEL_NAME}.ckpt")
-            model = FaceIDModel.load_from_checkpoint(checkpoint_path, model=model_core, train_loader=train_loader, val_loader=val_loader)
-        train_losses, val_losses = train_model(model)
-        plot_losses(train_losses, val_losses)
-    else:
-        checkpoint_path = MODELS_PATH + f"\\{MODEL_NAME}.ckpt"
-
-        model = FaceIDModel.load_from_checkpoint(
-            checkpoint_path=checkpoint_path,
-            model=model.model,
-            train_loader=train_loader,
-            val_loader=val_loader
-        )
-
-        criterion = nn.BCEWithLogitsLoss() if MODEL_NAME == 'classification' else nn.MSELoss()
-        model.criterion = criterion
-
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        model = model.to(device)
-
-        model.eval()
-        model.freeze()
-
-        all_predictions = []
-        all_targets = []
-
-        with torch.no_grad():
-            for batch in test_loader:
-                x, y = batch.x.to(device), batch.y.to(device)
-                edge_index = batch.edge_index.to(device)
-
-                outputs = model(batch.to(device))
-
-                if MODEL_NAME == 'classification':
-                    y = y.squeeze().long()
-                    loss = nn.CrossEntropyLoss()(outputs, y)
-                    probabilities = torch.softmax(outputs, dim=-1)
-                    predictions = probabilities.argmax(dim=-1)
-
-                elif MODEL_NAME == 'regression':
-                    y = y.squeeze().float()
-                    loss = nn.MSELoss()(outputs.squeeze(), y)
-                    predictions = outputs.squeeze()
-
-                all_predictions.append(predictions.cpu())
-                all_targets.append(y.cpu())
-
-        all_predictions = torch.cat(all_predictions)
-        all_targets = torch.cat(all_targets)
-
-        if MODEL_NAME == 'classification':
-            test_accuracy = (all_predictions == all_targets).float().mean().item() * 100
-            print(f"Test Accuracy: {test_accuracy:.4f}%")
-
-            cm = confusion_matrix(all_targets, all_predictions)
-            plt.figure(figsize=(8, 6))
-            sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
-            plt.xlabel('Predicted Labels')
-            plt.ylabel('True Labels')
-            plt.title('Confusion Matrix')
-            plt.show()
-
-        elif MODEL_NAME == 'regression':
-            mse = torch.mean((all_predictions - all_targets) ** 2).item()
-            print(f"Mean Squared Error (MSE): {mse:.4f}")
+    
+   main_transformer_conv()
+   main_gcn_conv()
--- Zadanie 3/src/model.py
+++ Zadanie 3/src/model.py
@@ -12,8 +12,8 @@ class GNNModelClassification(nn.Module):
 
         self.pool = global_mean_pool
 
-        self.fc1 = nn.Linear(embedding, 64)
-        self.fc2 = nn.Linear(64, 2)
+        self.fc1 = nn.Linear(embedding, embedding*2)
+        self.fc2 = nn.Linear(embedding*2, 2)
 
         self.relu = nn.ReLU()
         self.output_activation = nn.Sigmoid()
@@ -45,7 +45,7 @@ class GNNModelClassification(nn.Module):
         return self.output_activation(x)
 
 class GNNModelRegression(nn.Module):
-    def __init__(self, embedding: int = 32, linear: bool = True):
+    def __init__(self, linear, embedding: int = 32):
         super(GNNModelRegression, self).__init__()
 
         self.conv1 = TransformerConv(in_channels=11, out_channels=16)
@@ -55,14 +55,15 @@ class GNNModelRegression(nn.Module):
 
         if linear:
             self.fc1 = nn.Linear(embedding, 1)
+            self.fc2 = None
         else:
-            self.fc1 = nn.Linear(embedding, 64)
-            self.fc2 = nn.Linear(64,1)
+            self.fc1 = nn.Linear(embedding, embedding * 2)
+            self.fc2 = nn.Linear(embedding * 2, 1)
 
         self.relu = nn.ReLU()
         self.output_activation = nn.Identity()
 
-    def forward(self, data, linear: bool = True):
+    def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
 
         print(f"x type: {type(x)} | shape: {x.shape if isinstance(x, torch.Tensor) else 'Not a tensor'}")
@@ -83,7 +84,96 @@ class GNNModelRegression(nn.Module):
 
         x = self.pool(x, batch)
 
+        if self.fc2 is None:
+            x = self.relu(self.fc1(x))
+        else:
+            x = self.relu(self.fc1(x))
+            x = self.fc2(x)
+
+        return self.output_activation(x)
+    
+class GNNModelClassification_GCNConv(nn.Module):
+    def __init__(self, embedding: int = 32):
+        super(GNNModelClassification_GCNConv, self).__init__()
+
+        self.conv1 = GCNConv(in_channels=9, out_channels=16)
+        self.conv2 = GCNConv(in_channels=16, out_channels=embedding)
+
+        self.pool = global_mean_pool
+
+        self.fc1 = nn.Linear(embedding, embedding*2)
+        self.fc2 = nn.Linear(embedding*2, 2)
+
+        self.relu = nn.ReLU()
+        self.output_activation = nn.Sigmoid()
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        print(f"x type: {type(x)} | shape: {x.shape if isinstance(x, torch.Tensor) else 'Not a tensor'}")
+        
+        if not isinstance(x, torch.Tensor):
+            raise ValueError("Input x should be a tensor")
+        
+        if len(x.shape) != 2 or x.shape[1] != 9:
+            raise ValueError(f"Unexpected shape of x. Expected [num_nodes, 9] but got {x.shape}")
+
+        x = x.float()
+
+        if edge_index is None or batch is None:
+            raise ValueError("Input data must contain edge_index and batch.")
+
+        x = self.relu(self.conv1(x, edge_index))
+        x = self.relu(self.conv2(x, edge_index))
+
+        x = self.pool(x, batch)
+
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return self.output_activation(x)
+
+class GNNModelRegression_GCNConv(nn.Module):
+    def __init__(self, linear, embedding: int = 32):
+        super(GNNModelRegression_GCNConv, self).__init__()
+
+        self.conv1 = GCNConv(in_channels=11, out_channels=16)
+        self.conv2 = GCNConv(in_channels=16, out_channels=embedding) #Dodatkowo 1 i 2 na out_channels
+
+        self.pool = global_mean_pool
+
         if linear:
+            self.fc1 = nn.Linear(embedding, 1)
+            self.fc2 = None
+        else:
+            self.fc1 = nn.Linear(embedding, embedding * 2)
+            self.fc2 = nn.Linear(embedding * 2, 1)
+
+        self.relu = nn.ReLU()
+        self.output_activation = nn.Identity()
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        print(f"x type: {type(x)} | shape: {x.shape if isinstance(x, torch.Tensor) else 'Not a tensor'}")
+        
+        if not isinstance(x, torch.Tensor):
+            raise ValueError("Input x should be a tensor")
+        
+        if len(x.shape) != 2 or x.shape[1] != 11:
+            raise ValueError(f"Unexpected shape of x. Expected [num_nodes, 11] but got {x.shape}")
+
+        x = x.float()
+
+        if edge_index is None or batch is None:
+            raise ValueError("Input data must contain edge_index and batch.")
+
+        x = self.relu(self.conv1(x, edge_index))
+        x = self.relu(self.conv2(x, edge_index))
+
+        x = self.pool(x, batch)
+
+        if self.fc2 is None:
             x = self.relu(self.fc1(x))
         else:
             x = self.relu(self.fc1(x))
